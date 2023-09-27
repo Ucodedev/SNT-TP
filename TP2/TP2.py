8a912c05cc653d7a9a5aeb435a0f91650baca30e
Ulyssedev <ulyssecardon@icloud.com>
2023-09-27 20:14:59 +0200

    Ajout du TP2/TP2.py
    Ajout du TP2/TP2.py

---
+++ TP2/TP2.py
@@ -0,0 +1,9 @@
+nom=input('Entrez votre nom : ')
+prenom=input('Entrez votre prénom : ')
+classe=input('Entrez votre classe : ')
+groupe=input('Entrez votre groupe : ')
+print('Bonjour',prenom,nom,'vous êtes en',classe,'dans le groupe',groupe,'.')
+nb1=float (input('Entrez le nombre 1 : '))
+nb2=float (input('Entrez le nombre 2 : '))
+resultat=nb1+nb2
+print (resultat)
